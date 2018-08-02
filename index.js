e5029b9c03afe4b9a47724f527bdc37c76573f78
Adrian Romero <dylanromero94@gmail.com>
2018-08-02 00:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
   }
   return array;
 }
- function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
   }
-  return array;
+  return facts;  
 }
  function iLoveTheBeatles(number) {
   var array = [];
