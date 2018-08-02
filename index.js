a980f89296871efb82a529af7f70a57ead76d284
Adrian Romero <dylanromero94@gmail.com>
2018-08-02 00:35:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
   return array;
 }
-function johnLennonFacts(foo, bar) {
+function johnLennonFacts() {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"["foo, bar"]];
   var array = [];
   var i = 0;
